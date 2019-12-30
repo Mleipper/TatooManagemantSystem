ba06d043a891dfb6d30f39b16c9fae79290cc932
m <mleipper@hotmail.com>
2019-12-30 00:26:37 +0000

    Add project files.

---
+++ Models/DataBaseModels/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TatooManagemantSystem.Models.DataBaseModels
+{
+    public class Stock
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int LowStockQuantity { get; set;  }
+
+        public string Uri { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime LastOrderDate { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+
+    }
+}
