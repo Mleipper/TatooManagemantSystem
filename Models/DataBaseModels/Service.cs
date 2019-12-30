7434fa2bad5ca8d7d634f14864c139ef2d5b581f
m <mleipper@hotmail.com>
2019-12-30 00:38:14 +0000

    Thinking how to map out Db

---
+++ Models/DataBaseModels/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TatooManagemantSystem.Models.DataBaseModels
+{
+    public class Service
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        //List of components-- A Service is made up of several peices of stock 
+        //StockId 
+        //AmountUsed
+        //
+    }
+}
